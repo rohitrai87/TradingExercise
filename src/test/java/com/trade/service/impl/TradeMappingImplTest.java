d9c9a18ad1d7be114dfe03d6caa1a8df420fee50
rohitrai87 <42120072+rohitrai87@users.noreply.github.com>
2018-08-09 16:47:22 +0100

    Add files via upload

--- src/test/java/com/trade/service/impl/TradeMappingImplTest.java
+++ src/test/java/com/trade/service/impl/TradeMappingImplTest.java
@@ -11,9 +11,6 @@ import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
 import com.trade.beans.Trade;
 import com.trade.constants.TradeConstants;
 import com.trade.service.TradeMapping;
@@ -23,54 +20,90 @@ public class TradeMappingImplTest {
 	private TradeMapping objTradeMap;
 	private List<Trade> alTrades;
 	private Map<String, Map<String, BigDecimal>> hmMap;
-	
-	@Mock
 	private Trade objTrade;
 	
 	@Before
 	public void before() throws ParseException {
-		MockitoAnnotations.initMocks(this);
 		objTradeMap = new TradeMappingImpl();
 		alTrades = new ArrayList<Trade>();
-		Date revStlmntDate = TradeConstants.sdf.parse("30/07/2018");
 		
-		objTrade = new Trade("foo", "B",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),800,BigDecimal.valueOf(60.25));
-		objTrade.setRevStlmntDate(revStlmntDate);
-		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(800)));
+		objTrade = new Trade("foo", "B",BigDecimal.valueOf(0.23),"AED",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("27/07/2018"),800,BigDecimal.valueOf(100.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("29/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.23).multiply(BigDecimal.valueOf(100.25))).multiply(new BigDecimal(800)));
+		alTrades.add(objTrade);
+
+		objTrade = new Trade("foo", "B",BigDecimal.valueOf(0.40),"SAR",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("28/07/2018"),800,BigDecimal.valueOf(140.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("29/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.40).multiply(BigDecimal.valueOf(140.25))).multiply(new BigDecimal(800)));
 		alTrades.add(objTrade);
 		
-		objTrade = new Trade("foo", "S",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),1000,BigDecimal.valueOf(60.25));
-		objTrade.setRevStlmntDate(revStlmntDate);
-		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(1000)));
+		objTrade = new Trade("foo", "S",BigDecimal.valueOf(0.23),"AED",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("27/07/2018"),1000,BigDecimal.valueOf(100.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("29/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.23).multiply(BigDecimal.valueOf(100.25))).multiply(new BigDecimal(1000)));
+		alTrades.add(objTrade);
+
+		objTrade = new Trade("foo", "S",BigDecimal.valueOf(0.40),"SAR",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("28/07/2018"),1000,BigDecimal.valueOf(140.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("29/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.40).multiply(BigDecimal.valueOf(140.25))).multiply(new BigDecimal(1000)));
 		alTrades.add(objTrade);
 		
-		objTrade = new Trade("bar", "B",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),900,BigDecimal.valueOf(60.25));
-		objTrade.setRevStlmntDate(revStlmntDate);
-		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(900)));
+		objTrade = new Trade("bar", "B",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("29/07/2018"),900,BigDecimal.valueOf(70.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(70.25))).multiply(new BigDecimal(900)));
+		alTrades.add(objTrade);
+
+		objTrade = new Trade("bar", "B",BigDecimal.valueOf(0.50),"GBP",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),900,BigDecimal.valueOf(80.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.50).multiply(BigDecimal.valueOf(80.25))).multiply(new BigDecimal(900)));
 		alTrades.add(objTrade);
 		
-		objTrade = new Trade("bar", "S",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),1200,BigDecimal.valueOf(60.25));
-		objTrade.setRevStlmntDate(revStlmntDate);
-		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(1200)));
+		objTrade = new Trade("bar", "S",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),1200,BigDecimal.valueOf(70.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(70.25))).multiply(new BigDecimal(1200)));
+		alTrades.add(objTrade);
+
+		objTrade = new Trade("bar", "S",BigDecimal.valueOf(0.50),"GBP",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("28/07/2018"),1200,BigDecimal.valueOf(80.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.50).multiply(BigDecimal.valueOf(80.25))).multiply(new BigDecimal(1200)));
 		alTrades.add(objTrade);
 		
-		objTrade = new Trade("zoo", "B",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),2000,BigDecimal.valueOf(60.25));
-		objTrade.setRevStlmntDate(revStlmntDate);
-		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(2000)));
+		objTrade = new Trade("zoo", "B",BigDecimal.valueOf(0.65),"SGP",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),2000,BigDecimal.valueOf(60.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.65).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(2000)));
+		alTrades.add(objTrade);
+
+		objTrade = new Trade("zoo", "B",BigDecimal.valueOf(0.65),"SGP",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("29/07/2018"),2000,BigDecimal.valueOf(60.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.65).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(2000)));
 		alTrades.add(objTrade);
 		
-		objTrade = new Trade("zoo", "S",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),4500,BigDecimal.valueOf(60.25));
-		objTrade.setRevStlmntDate(revStlmntDate);
-		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(4500)));
+		objTrade = new Trade("zoo", "S",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("31/07/2018"),4500,BigDecimal.valueOf(70.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("31/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(70.25))).multiply(new BigDecimal(4500)));
+		alTrades.add(objTrade);
+
+		objTrade = new Trade("zoo", "S",BigDecimal.valueOf(0.50),"GBP",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),4500,BigDecimal.valueOf(80.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.50).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(4500)));
 		alTrades.add(objTrade);
 		
-		objTrade = new Trade("moo", "B",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),2300,BigDecimal.valueOf(60.25));
-		objTrade.setRevStlmntDate(revStlmntDate);
+		objTrade = new Trade("moo", "B",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("31/07/2018"),2300,BigDecimal.valueOf(60.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("31/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(2300)));
+		alTrades.add(objTrade);
+
+		objTrade = new Trade("moo", "B",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("28/07/2018"),2300,BigDecimal.valueOf(60.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
 		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(2300)));
 		alTrades.add(objTrade);
 		
+		objTrade = new Trade("noo", "S",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("29/07/2018"),400,BigDecimal.valueOf(60.25));
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
+		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(400)));
+		alTrades.add(objTrade);
+
 		objTrade = new Trade("noo", "S",BigDecimal.valueOf(0.30),"USD",TradeConstants.sdf.parse("28/07/2016"),TradeConstants.sdf.parse("30/07/2018"),400,BigDecimal.valueOf(60.25));
-		objTrade.setRevStlmntDate(revStlmntDate);
+		objTrade.setRevStlmntDate(TradeConstants.sdf.parse("30/07/2018"));
 		objTrade.setTrdAmount((BigDecimal.valueOf(0.30).multiply(BigDecimal.valueOf(60.25))).multiply(new BigDecimal(400)));
 		alTrades.add(objTrade);
 	}//End of before method
@@ -81,8 +114,8 @@ public class TradeMappingImplTest {
 		assertNotNull(hmMap);
 		assertTrue((hmMap.size() != 0));
 		assertEquals(4, hmMap.size());
-		assertEquals(1, hmMap.get(TradeConstants.INCOMING_SETTLEMENT_AMNT).size());
-		assertEquals(1, hmMap.get(TradeConstants.OUTGOING_SETTLEMENT_AMNT).size());
+		assertEquals(3, hmMap.get(TradeConstants.INCOMING_SETTLEMENT_AMNT).size());
+		assertEquals(3, hmMap.get(TradeConstants.OUTGOING_SETTLEMENT_AMNT).size());
 		assertEquals(4, hmMap.get(TradeConstants.INCOMING_ENTITY_RANK).size());
 		assertEquals(4, hmMap.get(TradeConstants.OUTGOING_ENTITY_RANK).size());
 	}//End of getTradeMapTest method
--- src/test/java/com/trade/service/impl/TradeProcessorImplTest.java
+++ src/test/java/com/trade/service/impl/TradeProcessorImplTest.java
@@ -8,8 +8,6 @@ import java.text.SimpleDateFormat;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import com.trade.beans.Trade;
 import com.trade.constants.TradeConstants;
@@ -18,8 +16,6 @@ import com.trade.service.TradeProcessor;
 public class TradeProcessorImplTest {
 	//Object under testing
 	private TradeProcessor objTradeProc;
-	
-	@Mock
 	private Trade objTrade1;
 	private Trade objTrade2;
 	private Trade objTrade3;
@@ -36,72 +32,112 @@ public class TradeProcessorImplTest {
 	
 	@Before
 	public void before() throws ParseException {
-		MockitoAnnotations.initMocks(this);
 		objTradeProc = new TradeProcessorImpl();
 		objTrade1 = new Trade("foo", "B",BigDecimal.valueOf(0.23),"AED",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("27/07/2018"),2000,BigDecimal.valueOf(100.25));
 		objTrade2 = new Trade("foo", "B",BigDecimal.valueOf(0.23),"AED",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("28/07/2018"),3000,BigDecimal.valueOf(100.25));
-		objTrade3 = new Trade("foo", "B",BigDecimal.valueOf(0.23),"AED",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("29/07/2018"),3000,BigDecimal.valueOf(100.25));
-		objTrade4 = new Trade("foo", "B",BigDecimal.valueOf(0.23),"SAR",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("27/07/2018"),2000,BigDecimal.valueOf(140.25));
-		objTrade5 = new Trade("foo", "B",BigDecimal.valueOf(0.23),"SAR",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("28/07/2018"),2000,BigDecimal.valueOf(140.25));
-		objTrade6 = new Trade("foo", "B",BigDecimal.valueOf(0.23),"SAR",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("29/07/2018"),2000,BigDecimal.valueOf(140.25));
-		objTrade7 = new Trade("foo", "B",BigDecimal.valueOf(0.50),"SGP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("28/07/2018"),1000,BigDecimal.valueOf(100.25));
-		objTrade8 = new Trade("foo", "B",BigDecimal.valueOf(0.50),"SGP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("29/07/2018"),1500,BigDecimal.valueOf(100.25));
-		objTrade9 = new Trade("foo", "B",BigDecimal.valueOf(0.50),"SGP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("30/07/2018"),1500,BigDecimal.valueOf(100.25));
-		objTrade10 = new Trade("foo", "B",BigDecimal.valueOf(1.70),"GBP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("28/07/2018"),2000,BigDecimal.valueOf(90.5));
-		objTrade11 = new Trade("foo", "B",BigDecimal.valueOf(1.70),"GBP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("29/07/2018"),2000,BigDecimal.valueOf(90.5));
-		objTrade12 = new Trade("foo", "B",BigDecimal.valueOf(1.70),"GBP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("30/07/2018"),2000,BigDecimal.valueOf(90.5));
+		objTrade3 = new Trade("foo", "B",BigDecimal.valueOf(0.23),"AED",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("29/07/2018"),1000,BigDecimal.valueOf(100.25));
+		objTrade4 = new Trade("foo", "B",BigDecimal.valueOf(0.40),"SAR",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("27/07/2018"),2000,BigDecimal.valueOf(140.25));
+		objTrade5 = new Trade("foo", "B",BigDecimal.valueOf(0.40),"SAR",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("28/07/2018"),5000,BigDecimal.valueOf(140.25));
+		objTrade6 = new Trade("foo", "B",BigDecimal.valueOf(0.40),"SAR",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("29/07/2018"),7000,BigDecimal.valueOf(140.25));
+		objTrade7 = new Trade("foo", "B",BigDecimal.valueOf(0.50),"SGP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("28/07/2018"),1000,BigDecimal.valueOf(150.25));
+		objTrade8 = new Trade("foo", "B",BigDecimal.valueOf(0.50),"SGP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("29/07/2018"),1500,BigDecimal.valueOf(150.25));
+		objTrade9 = new Trade("foo", "B",BigDecimal.valueOf(0.50),"SGP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("30/07/2018"),4500,BigDecimal.valueOf(150.25));
+		objTrade10 = new Trade("foo", "B",BigDecimal.valueOf(1.70),"GBP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("28/07/2018"),3500,BigDecimal.valueOf(90.5));
+		objTrade11 = new Trade("foo", "B",BigDecimal.valueOf(1.70),"GBP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("29/07/2018"),5500,BigDecimal.valueOf(90.5));
+		objTrade12 = new Trade("foo", "B",BigDecimal.valueOf(1.70),"GBP",TradeConstants.sdf.parse("26/07/2016"),TradeConstants.sdf.parse("30/07/2018"),4000,BigDecimal.valueOf(90.5));
 		dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 	}//End of before method
 	
+	//This methoid tests the logic when trade settlement date falls on their respective weekends for a currency
 	@Test
-	public void testProcessTrade() throws ParseException {
+	public void testProcessTradeOnWeekends() throws ParseException {
 		objTrade1 = objTradeProc.processTrade(objTrade1);
 		assertNotNull(objTrade1);
+		assertTrue((objTrade1.getStrCurr()).equals("AED"));
+		assertTrue((objTrade1.getOrigStlmntDate()).equals(dateFormat.parse("27/07/2018")));
 		assertTrue((objTrade1.getRevStlmntDate()).equals(dateFormat.parse("29/07/2018")));
+		assertEquals(objTrade1.getTrdAmount(), (BigDecimal.valueOf(0.23).multiply(BigDecimal.valueOf(100.25))).multiply(new BigDecimal(2000)));
 		
 		objTrade2 = objTradeProc.processTrade(objTrade2);
 		assertNotNull(objTrade2);
+		assertTrue((objTrade2.getStrCurr()).equals("AED"));
+		assertTrue((objTrade2.getOrigStlmntDate()).equals(dateFormat.parse("28/07/2018")));
 		assertTrue((objTrade2.getRevStlmntDate()).equals(dateFormat.parse("29/07/2018")));
-		
-		objTrade3 = objTradeProc.processTrade(objTrade3);
-		assertNotNull(objTrade3);
-		assertTrue((objTrade3.getRevStlmntDate()).equals(dateFormat.parse("29/07/2018")));
+		assertEquals(objTrade2.getTrdAmount(), (BigDecimal.valueOf(0.23).multiply(BigDecimal.valueOf(100.25))).multiply(new BigDecimal(3000)));
 		
 		objTrade4 = objTradeProc.processTrade(objTrade4);
 		assertNotNull(objTrade4);
+		assertTrue((objTrade4.getStrCurr()).equals("SAR"));
+		assertTrue((objTrade4.getOrigStlmntDate()).equals(dateFormat.parse("27/07/2018")));
 		assertTrue((objTrade4.getRevStlmntDate()).equals(dateFormat.parse("29/07/2018")));
+		assertEquals(objTrade4.getTrdAmount(), (BigDecimal.valueOf(0.40).multiply(BigDecimal.valueOf(140.25))).multiply(new BigDecimal(2000)));
 		
 		objTrade5 = objTradeProc.processTrade(objTrade5);
 		assertNotNull(objTrade5);
+		assertTrue((objTrade5.getStrCurr()).equals("SAR"));
+		assertTrue((objTrade5.getOrigStlmntDate()).equals(dateFormat.parse("28/07/2018")));
 		assertTrue((objTrade5.getRevStlmntDate()).equals(dateFormat.parse("29/07/2018")));
-		
-		objTrade6 = objTradeProc.processTrade(objTrade6);
-		assertNotNull(objTrade6);
-		assertTrue((objTrade6.getRevStlmntDate()).equals(dateFormat.parse("29/07/2018")));
+		assertEquals(objTrade5.getTrdAmount(), (BigDecimal.valueOf(0.40).multiply(BigDecimal.valueOf(140.25))).multiply(new BigDecimal(5000)));
 		
 		objTrade7 = objTradeProc.processTrade(objTrade7);
 		assertNotNull(objTrade7);
+		assertTrue((objTrade7.getStrCurr()).equals("SGP"));
+		assertTrue((objTrade7.getOrigStlmntDate()).equals(dateFormat.parse("28/07/2018")));
 		assertTrue((objTrade7.getRevStlmntDate()).equals(dateFormat.parse("30/07/2018")));
+		assertEquals(objTrade7.getTrdAmount(), (BigDecimal.valueOf(0.50).multiply(BigDecimal.valueOf(150.25))).multiply(new BigDecimal(1000)));
 		
 		objTrade8 = objTradeProc.processTrade(objTrade8);
 		assertNotNull(objTrade8);
+		assertTrue((objTrade8.getStrCurr()).equals("SGP"));
+		assertTrue((objTrade8.getOrigStlmntDate()).equals(dateFormat.parse("29/07/2018")));
 		assertTrue((objTrade8.getRevStlmntDate()).equals(dateFormat.parse("30/07/2018")));
-		
-		objTrade9 = objTradeProc.processTrade(objTrade9);
-		assertNotNull(objTrade9);
-		assertTrue((objTrade9.getRevStlmntDate()).equals(dateFormat.parse("30/07/2018")));
+		assertEquals(objTrade8.getTrdAmount(), (BigDecimal.valueOf(0.50).multiply(BigDecimal.valueOf(150.25))).multiply(new BigDecimal(1500)));
 		
 		objTrade10 = objTradeProc.processTrade(objTrade10);
 		assertNotNull(objTrade10);
+		assertTrue((objTrade10.getStrCurr()).equals("GBP"));
+		assertTrue((objTrade10.getOrigStlmntDate()).equals(dateFormat.parse("28/07/2018")));
 		assertTrue((objTrade10.getRevStlmntDate()).equals(dateFormat.parse("30/07/2018")));
+		assertEquals(objTrade10.getTrdAmount(), (BigDecimal.valueOf(1.70).multiply(BigDecimal.valueOf(90.5))).multiply(new BigDecimal(3500)));
 		
 		objTrade11 = objTradeProc.processTrade(objTrade11);
 		assertNotNull(objTrade11);
+		assertTrue((objTrade11.getStrCurr()).equals("GBP"));
+		assertTrue((objTrade11.getOrigStlmntDate()).equals(dateFormat.parse("29/07/2018")));
 		assertTrue((objTrade11.getRevStlmntDate()).equals(dateFormat.parse("30/07/2018")));
+		assertEquals(objTrade10.getTrdAmount(), (BigDecimal.valueOf(1.70).multiply(BigDecimal.valueOf(90.5))).multiply(new BigDecimal(5500)));
+		
+	}//End of testProcessTradeOnWeekends method
+
+	@Test
+	public void testProcessTradeOnWeekdays() throws ParseException {		
+		objTrade3 = objTradeProc.processTrade(objTrade3);
+		assertNotNull(objTrade3);
+		assertTrue((objTrade3.getStrCurr()).equals("AED"));
+		assertTrue((objTrade3.getOrigStlmntDate()).equals(dateFormat.parse("29/07/2018")));
+		assertTrue((objTrade3.getRevStlmntDate()).equals(dateFormat.parse("29/07/2018")));
+		assertEquals(objTrade3.getTrdAmount(), (BigDecimal.valueOf(0.23).multiply(BigDecimal.valueOf(100.25))).multiply(new BigDecimal(1000)));
+		
+		objTrade6 = objTradeProc.processTrade(objTrade6);
+		assertNotNull(objTrade6);
+		assertTrue((objTrade6.getStrCurr()).equals("SAR"));
+		assertTrue((objTrade6.getOrigStlmntDate()).equals(dateFormat.parse("29/07/2018")));
+		assertTrue((objTrade6.getRevStlmntDate()).equals(dateFormat.parse("29/07/2018")));
+		assertEquals(objTrade6.getTrdAmount(), (BigDecimal.valueOf(0.40).multiply(BigDecimal.valueOf(140.25))).multiply(new BigDecimal(7000)));
+		
+		objTrade9 = objTradeProc.processTrade(objTrade9);
+		assertNotNull(objTrade9);
+		assertTrue((objTrade9.getStrCurr()).equals("SGP"));
+		assertTrue((objTrade9.getOrigStlmntDate()).equals(dateFormat.parse("30/07/2018")));
+		assertTrue((objTrade9.getRevStlmntDate()).equals(dateFormat.parse("30/07/2018")));
+		assertEquals(objTrade9.getTrdAmount(), (BigDecimal.valueOf(0.50).multiply(BigDecimal.valueOf(150.25))).multiply(new BigDecimal(4500)));
 		
 		objTrade12 = objTradeProc.processTrade(objTrade12);
 		assertNotNull(objTrade12);
+		assertTrue((objTrade12.getStrCurr()).equals("GBP"));
+		assertTrue((objTrade12.getOrigStlmntDate()).equals(dateFormat.parse("30/07/2018")));
 		assertTrue((objTrade12.getRevStlmntDate()).equals(dateFormat.parse("30/07/2018")));
-	}//End of testProcessTrade method
+		assertEquals(objTrade12.getTrdAmount(), (BigDecimal.valueOf(1.70).multiply(BigDecimal.valueOf(90.5))).multiply(new BigDecimal(4000)));
+	}//End of testProcessTradeOnWeekdays method
 	
 }//End of TradeProcessorImplTest class
